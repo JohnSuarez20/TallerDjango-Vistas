1426856cd4a813010e7694a013cf38f8c45be574
JohnSuarez20 <j.serratos@uniandes.edu.co>
2023-02-11 15:52:29 -0500

    ModificacionesLab3

---
+++ measurements/logic/measurements_logic.py
@@ -0,0 +1,30 @@
+from ..models import Measurement
+
+def get_measurements():
+    measurements = Measurement.objects.all()
+    return measurements
+
+def get_measurement(var_pk):
+    measurement = Measurement.objects.get(pk=var_pk)
+    return measurement
+
+def delete_measurement(var_pk):
+    measurement = Measurement.objects.get(pk=var_pk)
+    measurement.delete()
+    
+
+def update_measurement(var_pk, new_var):
+    measurement = get_measurement(var_pk)
+    measurement.value = new_var["value"]
+    measurement.save()
+    return measurement
+
+def create_measurement(var):
+    measurement = Measurement(
+        variable_id=var["variable"],
+        value=var["value"],
+        unit=var["unit"],
+        place=var["place"],
+        dateTime=var["dateTime"])
+    measurement.save()
+    return measurement
